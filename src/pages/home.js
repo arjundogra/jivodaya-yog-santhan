8903ea08d9d1053fcb04fa76703bba278a452790
Arjun Dogra <66183261+arjundogra@users.noreply.github.com>
2020-11-06 23:58:44 +0530

    Add files via upload

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,6 +9,37 @@ function Home() {
           <h4>YOGAH KARMSHU KAUSHALAM</h4>
         </div>
       </div>
+      <div className="content">
+        <div className="content-container">
+          <h2>Yoga A Way Of Life</h2>
+          <div className="yogaContent">
+            <p>
+              Ut anim in laborum aute reprehenderit do nulla laborum. Ex nulla
+              in aliqua incididunt minim reprehenderit labore ea deserunt cillum
+              eiusmod pariatur occaecat. Duis veniam eu proident minim tempor
+              anim ea. Sit pariatur aliqua sunt adipisicing officia mollit
+              pariatur sint ut aute est. Ipsum occaecat reprehenderit est est id
+              culpa offi sunt magna amet laborum reprehenderit ex occaecat ut
+              aute aliquip irure laborum amet. Ex consequat do et aliquip non.
+              Ipsum ex sint nostrud quis. Aute sunt anim laboris reprehenderit.
+              Id sit proident aliqua laborum sint ullamco pariatur nulla Lorem
+              officia. In consequat aliquip reprehenderit amet et proident
+              mollit Lorem velit minim. Id enim consequat pariatur nulla aliquip
+              commodo aute voluptate laboris deserunt enim mollit deserunt quis.
+              Magna occaecat est quis sunt voluptate consectetur elit ad
+              occaecat velit. Consequat sint quis commodo amet ex exercitation
+              ullamco. Quis labore laborum aute irure elit est laboris nostrud.
+              Ullamco Lorem ut amet aute enim dolor. Magna laboris labore
+              officia Lorem dolor amet exercitation do qui nulla cillum sunt
+              cillum aliqua. Fugiat aliquip nisi voluptate duis sit tempor dolor
+              id. Aliqua ea in cupidatat occaecat duis dolor aute eiusmod esse
+              deserunt eiusmod. Ullamco Lorem anim reprehenderit cupidatat esse
+              fugiat occaecat.
+            </p>
+            <img src="https://scontent.fdel2-1.fna.fbcdn.net/v/t1.0-1/p720x720/10511125_270647149797570_6924102574775691431_n.jpg?_nc_cat=111&ccb=2&_nc_sid=dbb9e7&_nc_ohc=i1iYkjLllCoAX8FOt2P&_nc_ht=scontent.fdel2-1.fna&tp=6&oh=57db5853a6f8e9bb0a44a6ce8e9047d3&oe=5FCB4AB6" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
